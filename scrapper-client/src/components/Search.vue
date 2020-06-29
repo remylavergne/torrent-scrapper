91253bcf4105876d597fb59671e9eb6aed2dba82
Rémy Lavergne <lavergne.remy@gmail.com>
2020-06-29 22:44:06 +0200

    add loader during search

--- scrapper-client/src/components/Search.vue
+++ scrapper-client/src/components/Search.vue
@@ -3,9 +3,13 @@
     <h1>{{ msg }}</h1>
     <h1>{{ test }}</h1>
 
-    <div>
+    <div class="search-area-container">
+         <div class="loader-container" />
       <input v-model="request" type="text" />
       <button v-on:click="getQueryResults" class="search-button">search</button>
+      <div class="loader-container">
+          <div class="loader" v-if="isLoading"></div>
+      </div>
     </div>
 
     <div>
@@ -28,13 +32,16 @@ import { Torrent } from "@/models/Torrent.model";
 export default class Search extends Vue {
   @Prop() private msg!: string;
   @Prop() private test!: string;
+
   request = "";
-  results: Torrent[] = []; // Ma liste qui change
+  results: Torrent[] = [];
   displayList = false;
+  isLoading = false;
 
   public getQueryResults(): void {
     console.log("getQueryResults() called");
     if (this.request.length > 0) {
+      this.isLoading = true;
       console.log("request", this.request);
       fetch(`http://localhost:7070/classic-search/${this.request.trim()}`, {
         method: "GET"
@@ -47,10 +54,12 @@ export default class Search extends Vue {
             this.results = [...r]; // Update ici
             this.displayList = true;
             console.log(r);
+            this.isLoading = false;
           });
         },
         error => {
           console.log("error", error);
+          this.isLoading = false;
         }
       );
     }
@@ -83,30 +92,56 @@ input {
   box-sizing: border-box;
   margin-right: 10px;
 }
+.search-area-container {
+  display: inline-flex;
+  align-items: center;
+}
 .search-button {
-	box-shadow:inset 0px 1px 0px 0px #dcecfb;
-	background:linear-gradient(to bottom, #bddbfa 5%, #80b5ea 100%);
-	background-color:#bddbfa;
-	border-radius:6px;
-	border:1px solid #84bbf3;
-	display:inline-block;
-	cursor:pointer;
-	color:#ffffff;
-	font-family:Arial;
-	font-size:15px;
-	font-weight:bold;
-	padding:6px 24px;
-	text-decoration:none;
-	text-shadow:0px 1px 0px #528ecc;
+  box-shadow: inset 0px 1px 0px 0px #dcecfb;
+  background: linear-gradient(to bottom, #bddbfa 5%, #80b5ea 100%);
+  background-color: #bddbfa;
+  border-radius: 6px;
+  border: 1px solid #84bbf3;
+  display: inline-block;
+  cursor: pointer;
+  color: #ffffff;
+  font-family: Arial;
+  font-size: 15px;
+  font-weight: bold;
+  padding: 6px 24px;
+  text-decoration: none;
+  text-shadow: 0px 1px 0px #528ecc;
 }
 .search-button:hover {
-	background:linear-gradient(to bottom, #80b5ea 5%, #bddbfa 100%);
-	background-color:#80b5ea;
+  background: linear-gradient(to bottom, #80b5ea 5%, #bddbfa 100%);
+  background-color: #80b5ea;
 }
 .search-button:active {
-	position:relative;
-	top:1px;
+  position: relative;
+  top: 1px;
+}
+.loader-container {
+    width: 20px;
+    height: 20px;
+    margin-left: 10px;
+    align-content: center;
+    display: grid;
+}
+.loader {
+  border: 4px solid #f3f3f3; /* Light grey */
+  border-top: 4px solid #3498db; /* Blue */
+  border-radius: 50%;
+  width: 16px;
+  height: 16px;
+  animation: spin 2s linear infinite;
 }
 
-
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
 </style>
